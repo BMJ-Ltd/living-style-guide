5974ed1eb9cc729fcfbc8f82c407124efcee6086
aasante <aasante@bmj.com>
2014-08-13 16:00:32 +0100

    documentation page changes to start to reflect bmj styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,6 +38,7 @@ module.exports = function(grunt) {
           sourceMap: true,
           sourceMapIncludeSources: true
       },
+	  
       dist: {
         files: {
           'js/site.min.js': [
