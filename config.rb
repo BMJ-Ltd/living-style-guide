7530cd2950c2b892fa131193f85db7ffc54d0db3
james-skinner <jskinner@bmj.com>
2014-08-20 16:17:38 +0100

    New build system and project organisation.
    Initial reorganisation of source code
    
    Working build system!

--- config.rb
+++ config.rb
@@ -5,13 +5,15 @@ require 'breakpoint'
 require 'compass-recipes'
 require 'compass-normalize'
 require 'susy'
-require 'rgbapng'
+require 'rgba_png'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "bootflat/css"
-sass_dir = "bootflat/scss"
-
+css_dir = "dist/css"
+sass_dir = "src/scss"
+images_dir = "src/static/img"
+generated_images_dir = "dist/img"
+fonts_dir = "src/static/fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -26,6 +28,6 @@ relative_assets = true
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
-preferred_syntax = :sass
+preferred_syntax = :scss
 # and then run:
 # sass-convert -R --from scss --to sass scss scss && rm -rf sass && mv scss sass
---
+++ gulpfile.js
@@ -0,0 +1,160 @@
+
+var gulp              = require('gulp');
+var $                 = require('gulp-load-plugins')({ lazy: true });
+var merge             = require('merge-stream');
+var path              = require('path');
+
+
+// ----------------------------------------------------------------------------
+
+// Error notification settings for plumber
+var plumberErrorHandler = {
+  errorHandler: $.notify.onError({
+    title: 'Gulp',
+    message: "Error: <%= error.message %>"
+  })
+};
+
+
+// ----------------------------------------------------------------------------
+
+var config = {
+  src: {
+    scss: [
+      'src/scss/**/!(_)*.scss'
+    ],
+
+    css: {
+      preSass: [
+        'src/vendor/boot/dist/css/bootstrap.css',
+        'src/vendor/font-awesome/css/font-awesome.css',
+        'src/vendor/angularicons/css/angularicons.css'
+      ],
+      postSass: [
+        'src/css/**/*.css'
+      ]
+    },
+
+    js: [
+      'src/vendor/jquery/dist/jquery.js',
+      'src/vendor/boot/dist/js/bootstrap.js',
+      'src/vendor/iCheck/icheck.js',
+      'src/vendor/Stepper/jquery.fs.stepper.js',
+      'src/vendor/Selecter/jquery.fs.selecter.js',
+      'src/js/application.js'
+    ],
+
+    files: [
+      {
+        src: 'src/static/**/*.*',
+        base: 'src/static'
+      },
+      {
+        src: 'src/vendor/boot/dist/fonts/*.*',
+        base: 'src/vendor/boot/dist'
+      },
+      {
+        src: 'src/vendor/font-awesome/fonts/*.*',
+        base: 'src/vendor/font-awesome'
+      }
+    ]
+  }
+};
+
+
+// ----------------------------------------------------------------------------
+// Clean all build output
+
+gulp.task('clean', function() {
+  return gulp.src('dist', { read: false })
+    .pipe($.clean());
+});
+
+
+// ----------------------------------------------------------------------------
+// Styles
+
+gulp.task('styles', function() {
+  var scss = gulp.src(config.src.scss)
+    // .pipe($.plumber(plumberErrorHandler))
+    .pipe($.rubySass({
+      require: ['breakpoint'],
+      trace: true
+    }));
+
+  var cssPre = gulp.src(config.src.css.preSass),
+    cssPost = gulp.src(config.src.css.postSass);
+
+  return merge(cssPre, scss, cssPost)
+    // .pipe($.autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe($.concat('site.css'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe($.rename({ suffix: '.min' }))
+    .pipe($.minifyCss())
+    .pipe(gulp.dest('dist/css'));
+});
+
+
+// ----------------------------------------------------------------------------
+// Scripts
+
+gulp.task('scripts', function() {
+  return gulp.src(config.src.js)
+    // .pipe($.plumber(plumberErrorHandler))
+    .pipe($.concat('site.js'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe($.uglify())
+    .pipe($.rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist/js'));
+});
+
+
+// ----------------------------------------------------------------------------
+// Static files
+
+gulp.task('files', function() {
+  var sources = config.src.files.map(function(spec) {
+    return gulp.src(spec.src, { base: spec.base });
+  });
+
+  return merge(sources)
+    .pipe($.newer('dist'))
+    .pipe(gulp.dest('dist'));
+});
+
+
+// ----------------------------------------------------------------------------
+
+//watch
+gulp.task('watch', ['build'], function() {
+  // livereload.listen();
+
+  var files = config.src.files.reduce(function(list, cur) {
+    return list.concat(typeof cur.src === 'string' ? [cur.src] : cur.src);
+  }, []);
+
+  gulp.watch([ config.src.scss, 'src/css/**/*.css' ],     [ 'styles' ]);
+  gulp.watch(config.src.js,                               [ 'scripts' ]);
+  gulp.watch(files,                                       [ 'files' ]);
+
+  //reload when a template file, the minified css, or the minified js file changes
+  // gulp.watch('templates/**/*.html', 'dist/css/styles.min.css', 'dist/js/main.min.js', function(event) {
+  //   gulp.src(event.path)
+  //     .pipe($.plumber())
+  //     .pipe($.livereload())
+  //     .pipe($.notify({
+  //       title: 'Gulp',
+  //       // icon: notifyInfo.icon,
+  //       message: event.path.replace(__dirname, '').replace(/\\/g, '/') + ' was ' + event.type + ' and reloaded'
+  //     })
+  //   );
+  // });
+});
+
+
+// ----------------------------------------------------------------------------
+
+gulp.task('build', ['files', 'scripts', 'styles']);
+
+gulp.task('default', ['watch']);
+
